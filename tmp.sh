cf569c002da0c39adfb2649f4c9c0ba08fc17058
zongou <zongou@outlook.com>
2024-08-06 07:13:47 +0800

    udpate

--- tmp.sh
+++ tmp.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+cat <<EOF >my_profile.sh
+export CHARSET=\${CHARSET:-UTF-8}
+export LANG=\${LANG:-C.UTF-8}
+export LC_COLLATE=\${LC_COLLATE:-C}
+export TERM=xterm-256color
+export COLORTERM=truecolor
+EOF
 
-cat 
+mv my_profile.sh /etc/profile.d/my_profile.sh
