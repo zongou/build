2ccec294e476df5fbcf6590049f6454feb02bd67
zongou <zongou@outlook.com>
2024-08-10 15:26:55 +0800

    update

--- tmp.sh
+++ tmp.sh
@@ -1,9 +1,4 @@
-cat <<EOF >my_profile.sh
-export CHARSET=\${CHARSET:-UTF-8}
-export LANG=\${LANG:-C.UTF-8}
-export LC_COLLATE=\${LC_COLLATE:-C}
-export TERM=xterm-256color
-export COLORTERM=truecolor
-EOF
+#!/bin/sh
+set -eu
 
-mv my_profile.sh /etc/profile.d/my_profile.sh
+echo Hello
