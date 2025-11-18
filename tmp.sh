1edffd3400c8e490580e0f4737571435010bb2f1
zongou <zongou@outlook.com>
2025-11-18 15:02:52 +0000

    u

--- tmp.sh
+++ tmp.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -eu
 
-echo Hello
+          VERSION=25.07.1
+          echo "Downloading cloudflared ${VERSION}"
+          ARCH=$(uname -m)
+          URL="https://github.com/helix-editor/helix/releases/download/${VERSION}/helix-${VERSION}-${ARCH}-linux.tar.xz"
+          curl -L "${URL}" | xz -d | tar -C /opt -x
+          ln -snf /opt/helix-${URL}-${ARCH}-linux/hx /usr/local/bin/hx
+          hx --version
